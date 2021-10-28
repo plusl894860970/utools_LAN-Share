5a51a46a9055695d8ffd87c9f85db43899c66581
plus <894860970@qq.com>
2021-10-28 13:00:30 +0800

    feat: 分片下载

--- utools/preload.js
+++ utools/preload.js
@@ -44,11 +44,12 @@ const startServer = async () => {
     }
     console.log('下载文件', JSON.stringify(file))
     // 下载文件
-    const { size } = fs.statSync(file.path);
-    const createReadStream = fs.createReadStream(file.path)
-    ctx.set('Content-type', 'application/force-download')
-    ctx.set('Content-Length', size)
-    ctx.body = createReadStream
+    let resHred = readFile(ctx.headers.range, file.path);
+    ctx.status = resHred.code
+    ctx.set(resHred.head);
+    let stream = fs.createReadStream(file.path, resHred.code == 200 ? {} : { start: resHred.start, end: resHred.end });
+    stream.pipe(ctx.res);
+    ctx.respond = false;
   })
   router.get('/files', async ctx => {
     ctx.body = await values();
@@ -136,6 +137,78 @@ const setList = (list) => {
 const { getIPAddresses } = require('./util');
 
 const getPort = async () => (port)
+
+
+/**
+ * [读文件]
+ * @param  {String} range        [数据起始位]
+ * @param  {String} filePath     [文件路径]
+ * @param  {Number} chunkSize    [每次请求碎片大小 （900kb 左右）]
+ */
+function readFile(range, filePath, chunkSize = 499999 * 2) {
+  //mime类型
+  const mime = {
+    "css": "text/css",
+    "gif": "image/gif",
+    "html": "text/html",
+    "ico": "image/x-icon",
+    "jpeg": "image/jpeg",
+    "jpg": "image/jpeg",
+    "js": "text/javascript",
+    "json": "application/json",
+    "pdf": "application/pdf",
+    "png": "image/png",
+    "svg": "image/svg+xml",
+    "swf": "application/x-shockwave-flash",
+    "tiff": "image/tiff",
+    "txt": "text/plain",
+    "mp3": "audio/mp3",
+    "wav": "audio/x-wav",
+    "wma": "audio/x-ms-wma",
+    "wmv": "video/x-ms-wmv",
+    "xml": "text/xml",
+    "mp4": "video/mp4"
+  };
+  // 获取后缀名
+  let ext = path.extname(filePath);
+  ext = ext ? ext.slice(1) : 'unknown';
+  //未知的类型一律用"text/plain"类型
+  let contentType = mime[ext.toLowerCase()];
+
+  //建立流对象，读文件
+  let stat = fs.statSync(filePath)
+  let fileSize = stat.size;
+  let head = {
+    code: 200,
+    head: {
+      'Content-Length': fileSize,
+      'content-type': contentType,
+    }
+
+  };
+  if (range) {
+    // 大文件分片
+    let parts = range.replace(/bytes=/, "").split("-");
+    let start = parseInt(parts[0], 10);
+    let end = parts[1] ? parseInt(parts[1], 10) : start + chunkSize;
+    end = end > fileSize - 1 ? fileSize - 1 : end;
+    chunkSize = (end - start) + 1;
+    head = {
+      code: 206,
+      filePath,
+      start,
+      end,
+      head: {
+        'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+        'content-type': contentType,
+        'Content-Length': chunkSize,
+        'Accept-Ranges': 'bytes'
+      }
+    }
+
+  }
+  return head;
+}
 
 window.api = {
   startServer,
